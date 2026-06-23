1b6f4719cbbc6bf446c9ed42bdb7edd33661b9c5
unknown <dudgusdl112@gmail.com>
2026-06-23 15:09:33 +0900

    Initial commit : React + Vitesetup

---
+++ f1-backend/f1-backend/src/test/java/f1_backend/F1BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package f1_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F1BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
